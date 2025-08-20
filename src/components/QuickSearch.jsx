314c6899b02e0c9f69a6cb16edc11fb52ab92cd1
bhaveshthakurstl <bhavesh.thakur@stl.tech>
2025-08-20 21:13:10 +0530

    Quick-search

--- src/components/QuickSearch.jsx
+++ src/components/QuickSearch.jsx
@@ -5,7 +5,9 @@ export default function QuickSearch() {
   return (
     <div className='quicksearch-body'>
          <div class="quick-container">
-        <div className='quick-title'>Quick Search</div>
+        <div className='quick-heading'>
+            <div className='quick-title'>Quick Search</div>
+        </div>
         
         <div class="quick-search-row">
             <div class="quick-dropdown">
@@ -15,14 +17,14 @@ export default function QuickSearch() {
                 </svg>
             </div>
 
-            <div class="dropdown placeholder">
+            <div class="quick-dropdown placeholder">
                 <span>Select Department</span>
                 <svg class="chevron" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                 </svg>
             </div>
 
-            <div class="dropdown placeholder">
+            <div class="quick-dropdown placeholder">
                 <span>Select Doctor</span>
                 <svg class="chevron" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
