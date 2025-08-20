248bf311f6d0ac954537056f0daed93e65b79099
bhaveshthakurstl <bhavesh.thakur@stl.tech>
2025-08-20 15:48:47 +0530

    global network

--- src/components/AsterGlobalNetwork.jsx
+++ src/components/AsterGlobalNetwork.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './AsterGlobalNetwork.css'
+import globalImage from '../assets/converted_image.jpg'
 
 const AsterGlobalNetwork = () => {
   return (
-    <div className='body'>
-        <div class="container">
-        <div class="content-heading">
+    <div className='global-body'>
+        <div class=".global-container">
+        <div class="global-content-heading">
             <h1>Aster Global Network</h1>
         </div>
 
-        <div class="main-content">
+        {/* <div class="main-content">
             <div class="stats-section">
                 <div class="stat-card large">
                     <span class="stat-number" data-target="7">7</span>
@@ -40,31 +41,18 @@ const AsterGlobalNetwork = () => {
             <div class="globe-section">
                 <div class="globe-container">
                     <div class="globe-grid"></div>
-                    
-                    {/* <div class="location-pin pin-1" ></div>
-                    <div class="location-pin pin-2" ></div>
-                    <div class="location-pin pin-3" ></div>
-                    <div class="location-pin pin-4" ></div>
-                    <div class="location-pin pin-5" ></div>
-                    <div class="location-pin pin-6" ></div>
-                    <div class="location-pin pin-7" ></div>
-
-                    <div class="connection-lines">
-                        <div class="connection-line" style="top: 25%; left: 25%; width: 100px; transform: rotate(45deg);"></div>
-                        <div class="connection-line" style="top: 35%; left: 45%; width: 80px; transform: rotate(-30deg);"></div>
-                        <div class="connection-line" style="top: 50%; left: 35%; width: 90px; transform: rotate(60deg);"></div>
-                    </div> */}
                 </div>
 
                 <div class="floating-stats floating-1">Excellence in Healthcare</div>
                 <div class="floating-stats floating-2">Serving Millions</div>
                 <div class="floating-stats floating-3">Global Presence</div>
             </div>
-        </div>
+        </div> */}
+        <img src={globalImage} alt='global' style={{height: '400px', width: '1400px'}}/>
 
-        <div class="update-info">*Last Updated on August 14 2023</div>
+        {/* <div class="update-info">*Last Updated on August 14 2023</div>
 
-        <div class="interactive-tooltip" id="tooltip"></div>
+        <div class="interactive-tooltip" id="tooltip"></div> */}
     </div>
     </div>
   )
